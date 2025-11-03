596a014b86c66867eba30e036d5fc2cbc475f9ed
Thiago Waib Castello Branco <thiago.waib@dsin.com.br>
2025-11-03 08:35:30 -0300

    Init

---
+++ android/app/src/main/kotlin/com/thiagowaib/bmi_calculator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.thiagowaib.bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: InputPage());
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("BMI CALCULATOR")),
+      body: Center(child: Text('Body Text')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
